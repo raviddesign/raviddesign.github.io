f3be06c6fa9fdcddc9bee773b5f5f8e9a8b62e90
MichaelRavid <Michael.Ravid@hotmail.com>
2018-02-07 07:08:19 -0500

    February Deployment
    February deployment with new content

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,29 @@
 //Animation for header
 
-var i =1;
+// var i =1;
 
-function func(){
-	var message="Responsive Web Design"
-	if (i <= message.length){
-		var txt = message.substring(0,i);
-		document.getElementById("home-text").innerHTML = txt;
-		setTimeout("func()",150)
-		i++
-	}
-	else{
-	}
+// function func(){
+// 	var message="Responsive Web Design"
+// 	if (i <= message.length){
+// 		var txt = message.substring(0,i);
+// 		document.getElementById("home-text").innerHTML = txt;
+// 		setTimeout("func()",150)
+// 		i++
+// 	}
+// 	else{
+// 	}
+// }
+// func();
+
+//No scroll
+document.addEventListener('touchstart', this.touchstart);
+document.addEventListener('touchmove', this.touchmove);
+
+function touchstart(e) {
+    e.preventDefault()
 }
-func();
+
+function touchmove(e) {
+    e.preventDefault()
+}
 
